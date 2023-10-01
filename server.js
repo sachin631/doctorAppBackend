49632dcabce43db10179f954a3a7160d9a3f9d3f
sachin <sangwansachin631@gmail.com>
2023-10-01 10:06:04 +0530

    a

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ try {
   );
   // Handle preflight (OPTIONS) requests
   app.options("*", cors()); // Enable CORS for all OPTIONS requests
-  
+  // app.use(cors())
   app.use(express.json({ limit: "10mb" }));
   // app.use(cookieParser());
   // app.use(bodyParser.urlencoded({ extended: true }));
